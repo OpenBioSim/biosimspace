50d33e30696c88595e9df2816f339894239ae60c
Lester Hedges <lester.hedges@gmail.com>
2025-01-07 13:40:30 +0000

    Update copyright range. [ci skip]

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -43,7 +43,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "biosimspace"
-copyright = "2017-2024"
+copyright = "2017-2025"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
